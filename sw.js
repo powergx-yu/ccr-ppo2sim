fd0f0343ed7ed55d3e109176547fc6cac31a1fa5
Myron Yu <myronredknarp@hotmail.com>
2026-01-30 00:28:49 +0900

    Update UI: relocate install button, add PowergxIcon, implement trilingual instructions, and clean up toast notification

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'ccr-sim-v10';
+const ASSETS_TO_CACHE = [
+    './index.html',
+    './manifest.json',
+    './icon.jpg'
+];
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE).catch(err => {
+                console.warn('Cache addAll failed:', err);
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then(networkResponse => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
